aea707323820c59d7026c9d82315a6ed7cae1f91
ironpup <christoph.ding@gmail.com>
2015-11-05 12:21:26 -0800

    Started MongoDb / Mongoose refactor;

--- app/config.js
+++ app/config.js
@@ -1,45 +1,36 @@
-var Bookshelf = require('bookshelf');
+var Mongoose = require('mongoose');
 var path = require('path');
 
-var db = Bookshelf.initialize({
-  client: 'sqlite3',
-  connection: {
-    host: '127.0.0.1',
-    user: 'your_database_user',
-    password: 'password',
-    database: 'shortlydb',
-    charset: 'utf8',
-    filename: path.join(__dirname, '../db/shortly.sqlite')
-  }
+var db = Mongoose.connect('mongodb://localhost/shortly', {
+  user: 'your_database_user',
+  pass: 'password'
+})
+
+var Schema = Mongoose.Schema;
+
+db.urls = new Schema({
+  url : String,
+  base_url: String,
+  code: String,
+  title: String,
+  visits: Number,
+  date: {type: Date, default: Date.now}
+}
+
+db.users = new Schema({
+  username: String,
+  password: String
 });
 
-db.knex.schema.hasTable('urls').then(function(exists) {
-  if (!exists) {
-    db.knex.schema.createTable('urls', function (link) {
-      link.increments('id').primary();
-      link.string('url', 255);
-      link.string('base_url', 255);
-      link.string('code', 100);
-      link.string('title', 255);
-      link.integer('visits');
-      link.timestamps();
-    }).then(function (table) {
-      console.log('Created Table', table);
-    });
-  }
-});
-
-db.knex.schema.hasTable('users').then(function(exists) {
-  if (!exists) {
-    db.knex.schema.createTable('users', function (user) {
-      user.increments('id').primary();
-      user.string('username', 100).unique();
-      user.string('password', 100);
-      user.timestamps();
-    }).then(function (table) {
-      console.log('Created Table', table);
-    });
-  }
-});
+db.users.pre('save', function() {
+  var user = this;
+    bcrypt.hash(user.password, null, null, function(err, hash){
+      if(err) {
+        console.log(err);      
+      } else {
+        user.password = hash;
+    }
+  })  
+}
 
 module.exports = db;
